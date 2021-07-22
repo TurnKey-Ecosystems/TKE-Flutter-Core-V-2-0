0ce7915eaa9e1c9f9b09ced48fd5f8e4cddf92cf
MechMel <MelchiahMauck@gmail.com>
2021-07-22 15:52:07 -0700

    Commented out tu.dart for now.

--- UI/tu.dart
+++ UI/tu.dart
@@ -1,7 +1,7 @@
-import 'package:tke_dev_time_tracker_flutter_tryw/TKE-Flutter-Core/UI/TFC_AppStyle.dart';
+/*import 'package:tke_dev_time_tracker_flutter_tryw/TKE-Flutter-Core/UI/TFC_AppStyle.dart';
 
 class tu {
   double operator *(num other) {
     return TFC_AppStyle.instance.lineHeight * other.toDouble();
   }
-}
+}*/
